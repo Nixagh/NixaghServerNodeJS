52266e31a876797fee868e34e28c8d240fae740b
Nixagh <nghia.nguyen-dinh@banvien.com.vn>
2024-03-30 06:42:51 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const env = require('./commons/envStorage');
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(env.server.port, () => {
+    console.log(`Example app listening on port ${env.server.port}`)
+})
