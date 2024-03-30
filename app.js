1eb4c86ce731955c570e57dbdc62e199d557ec09
Nixagh <nghia.nguyen-dinh@banvien.com.vn>
2024-03-30 07:05:31 +0700

    add nodemon

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const env = require('./commons/envStorage');
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
-})
+});
 
 app.listen(env.server.port, () => {
     console.log(`Example app listening on port ${env.server.port}`)
-})
+});
